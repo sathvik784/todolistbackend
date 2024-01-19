a9d8c7b0b514f85c6edf2ab777704b4e515c2f27
Sathvik Andhavarapu <sandhav1@terpmail.umd.edu>
2024-01-19 17:02:39 +0530

    adding manager field to task

--- todolist/models.py
+++ todolist/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Task(models.Model):
+    manager = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     task = models.CharField(max_length=500)
     done = models.BooleanField(default=False)
